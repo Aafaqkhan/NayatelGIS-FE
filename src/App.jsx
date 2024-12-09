427e9e8864d0335fa5cc0d42c3b8d2c69210f757
Aafaqkhan <itsafaqkhan@gmail.com>
2024-12-09 16:18:18 +0500

    basic setup changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
-import LoginPage from './LoginScreen/LoginPage'
+import LoginPage from './Pages/LoginScreen/LoginPage'
 
 function App() {
   // const [count, setCount] = useState(0)
