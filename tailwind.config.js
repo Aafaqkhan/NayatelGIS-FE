da0a601f506d48647e6d73f4e56ced18751ac0a7
Aafaqkhan <48995104+Aafaqkhan@users.noreply.github.com>
2024-12-30 14:52:33 +0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,11 @@
 module.exports = {
-    darkMode: "class",
-    content: [
-        './pages/**/*.{js,ts,jsx,tsx}',
-        './components/**/*.{js,ts,jsx,tsx}',
-        './node_modules/tw-elements/dist/js/**/*.{js,ts,jsx,tsx}',
-    ],
+     
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
         extend: {},
     },
     plugins: [
-        require('tw-elements/dist/plugin')
+        
     ], }
 
 // /** @type {import('tailwindcss').Config} */
